a968b7164cdd01319c23f7f2eb0a9d4a5a0601f6
Łukasz Ambrożewski <lukaszambrozewski@wp.pl>
2022-12-31 07:54:19 +0100

    Added buttons for home section.

---
+++ src/components/HomeSection/HomeBtn/HomeBtn.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./HomeBtn.scss";
+
+type MainBtnProps = {
+    text: string;
+    transparent?: boolean;
+    onClick: () => void;
+};
+
+export const HomeBtn = ({text, transparent, onClick}: MainBtnProps) => {
+    return (
+        <button
+            className={transparent ? "home-btn home-btn__transparent" : "home-btn"}
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    );
+};
