6d0996c1d3ed02e9a8038f748a2645f5214b1b16
Łukasz Ambrożewski <lukaszambrozewski@wp.pl>
2023-01-01 18:10:17 +0100

    Added ChooseUsBox component.

--- src/components/AboutUsSection/ServiceBox/ServiceBox.tsx
+++ src/components/AboutUsSection/ServiceBox/ServiceBox.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import "./ServiceBox.scss";
 
 type ServicesBoxProps = {
-    isBlue?: boolean;
-    onClick?: () => void;
-    icon: string;
-    text: string;
+  isBlue?: boolean;
+  onClick?: () => void;
+  icon: string;
+  text: string;
 };
 
-export const ServiceBox = ({isBlue, icon, text}: ServicesBoxProps) => {
-    return (
-        <div className={isBlue ? "service__box service__blue" : "service__box"}>
-            <img src={icon} alt="Service icon"/>
-            <br/>
-            {text}
-        </div>
-    );
+export const ServiceBox = ({ isBlue, icon, text }: ServicesBoxProps) => {
+  return (
+    <div className={isBlue ? "service__box service__blue" : "service__box"}>
+      <img src={icon} alt="Service icon" />
+      <br />
+      {text}
+    </div>
+  );
 };
