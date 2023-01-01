ed2d0d0846b28f040b42deee29824a7a59657f75
Łukasz Ambrożewski <lukaszambrozewski@wp.pl>
2023-01-01 13:31:31 +0100

    Added navigation.

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+import {Header} from "../../components/Header/Header";
+import {HomeSection} from "../../components/HomeSection/HomeSection";
+import {AboutUsSection} from "../../components/AboutUsSection/AboutUsSection";
+import "../../style/main.scss";
 
 export const Main = () => {
-    return(
-        <div>Main</div>
-    )
-}
+    return (
+        <div className="main-view">
+            <section id="header">
+                <Header/>
+            </section>
+            <section id="home">
+                <HomeSection/>
+            </section>
+            <section id="about-us">
+                <AboutUsSection/>
+            </section>
+        </div>
+    );
+};
