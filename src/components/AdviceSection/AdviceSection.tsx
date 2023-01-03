483082fc13a6595dc0d68499dbfe798be33b3672
Łukasz Ambrożewski <lukaszambrozewski@wp.pl>
2023-01-03 14:29:18 +0100

    Added PracticeAdviceBox component to AdiviceSection.

---
+++ src/components/AdviceSection/AdviceSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../style/main.scss";
+import { PracticeAdviceBox } from "./PracticeAdviceBox/PracticeAdviceBox";
+
+export const AdviceSection = () => {
+  return (
+    <div className="advice-section">
+      <PracticeAdviceBox />
+    </div>
+  );
+};
