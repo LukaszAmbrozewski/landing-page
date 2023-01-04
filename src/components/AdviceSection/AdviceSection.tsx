27379de66db81d4ba9800141275b07002ac72ad8
Łukasz Ambrożewski <lukaszambrozewski@wp.pl>
2023-01-04 08:38:27 +0100

    Added NewsletterBox component.

--- src/components/AdviceSection/AdviceSection.tsx
+++ src/components/AdviceSection/AdviceSection.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../../style/main.scss";
 import { PracticeAdviceBox } from "./PracticeAdviceBox/PracticeAdviceBox";
 import { PartnersBox } from "./PartnersBox/PartnersBox";
+import { NewsletterBox } from "./NewsletterBox/NewsletterBox";
 
 export const AdviceSection = () => {
   return (
     <div className="advice-section">
       <PracticeAdviceBox />
       <PartnersBox />
+      <NewsletterBox />
     </div>
   );
 };
