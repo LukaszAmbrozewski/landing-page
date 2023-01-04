986deea0e2ad1d9d396c388a0e07506ee706dddc
Łukasz Ambrożewski <lukaszambrozewski@wp.pl>
2023-01-04 11:31:31 +0100

    Added consulting box.

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../style/main.scss";
 import { GetInTouchBox } from "./GetInTouchBox/GetInTouchBox";
+import { ConsultingBox } from "./ConsultingBox/ConsultingBox";
 
 export const ContactSection = () => {
   return (
     <div className="contact-section">
       <GetInTouchBox />
+      <ConsultingBox />
     </div>
   );
 };
